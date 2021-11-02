2b63d0a08ef4b8db4ce8e868bc1c0a3c3d66e123
Calvin Koder <calvin.koder@yahoo.com>
2021-11-02 12:58:49 +1100

    Notes feature
    Added notes feature but currently uses shared preferences to store data and need to link it to the sidenav

---
+++ app/src/main/java/au/edu/unsw/infs3634/unswgamifiedlearningapp/NotesEditor.java
@@ -0,0 +1,69 @@
+package au.edu.unsw.infs3634.unswgamifiedlearningapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+import java.util.HashSet;
+
+
+//Source: https://www.youtube.com/watch?v=48EB4HeP1kI&fbclid=IwAR1ZZ8XrB1zuAcvkT3vG6Y1XsoJ6zH-TeWk0ZdwWUTCfUzLToSx6F2_Xle4
+
+public class NotesEditor extends AppCompatActivity {
+    int noteID;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notes_editor);
+
+        EditText editText = findViewById(R.id.editText);
+
+        Intent intent = getIntent();
+        noteID = intent.getIntExtra("noteID",-1);
+
+
+        //work out whether a new note or an old note (editnotes)
+        if (noteID != -1){
+
+            editText.setText(Notes.notes.get(noteID));
+        } else {
+            Notes.notes.add("");
+            noteID = Notes.notes.size() - 1;
+            Notes.arrayAdapter.notifyDataSetChanged();
+        }
+
+
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                Notes.notes.set(noteID,String.valueOf(s));
+                Notes.arrayAdapter.notifyDataSetChanged();
+
+                SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("au.edu.unsw.infs3634.unswgamifiedlearningapp", Context.MODE_PRIVATE);
+
+                HashSet<String> noteSet = new HashSet(Notes.notes);
+                sharedPreferences.edit().putStringSet("notes",noteSet).apply();
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+}
