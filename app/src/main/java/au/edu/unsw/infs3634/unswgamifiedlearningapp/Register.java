421a58324dfdf2ae3a9f9745312932edc790febe
cathngo <cngo1080@gmail.com>
2021-11-11 16:16:26 +1100

    Implemented room database for users

--- app/src/main/java/au/edu/unsw/infs3634/unswgamifiedlearningapp/Register.java
+++ app/src/main/java/au/edu/unsw/infs3634/unswgamifiedlearningapp/Register.java
@@ -2,12 +2,15 @@ package au.edu.unsw.infs3634.unswgamifiedlearningapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Database;
+import androidx.room.Room;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.widget.Button;
-import android.widget.EditText;
+
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,26 +20,36 @@ import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.List;
+import java.util.concurrent.Executors;
+
 //Reference for sign up using Firebase: https://www.youtube.com/watch?v=iSsa9OlQJms&ab_channel=SoftCoding
 
 public class Register extends AppCompatActivity {
 
     TextInputEditText etRegEmail;
     TextInputEditText  etRegPassword;
+    TextInputEditText  editFirstName;
+    TextInputEditText  editLastName;
     TextView txtLogin;
     Button btnRegister;
 
     FirebaseAuth mAuth;
 
+    private DatabaseAll mDb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+
+
         etRegEmail = findViewById(R.id.editEmailReg);
         etRegPassword = findViewById(R.id.editPasswordReg);
         txtLogin = findViewById(R.id.txtLogin);
         btnRegister = findViewById(R.id.btnRegister);
+        editFirstName = findViewById(R.id.editFirstName);
+        editLastName = findViewById(R.id.editLastName);
 
         mAuth = FirebaseAuth.getInstance();
 
@@ -49,11 +62,17 @@ public class Register extends AppCompatActivity {
         });
 
 
+        mDb = Room.databaseBuilder(getApplicationContext(), DatabaseAll.class, "database-all")
+                .fallbackToDestructiveMigration()
+                .build();
+
     }
 
     private void createUser(){
         String email = etRegEmail.getText().toString();
         String password = etRegPassword.getText().toString();
+        String fName = editFirstName.getText().toString();
+        String lName = editLastName.getText().toString();
 
         if (TextUtils.isEmpty(email)){
             etRegEmail.setError("Email cannot be empty");
@@ -67,6 +86,10 @@ public class Register extends AppCompatActivity {
                 public void onComplete(@NonNull Task<AuthResult> task) {
                     if (task.isSuccessful()){
                         Toast.makeText(Register.this, "User registered successfully", Toast.LENGTH_SHORT).show();
+                        //insert into room Dao
+                        DatabaseAll db  = DatabaseAll.getDbInstance(Register.this);
+                        String id = mAuth.getCurrentUser().getUid();
+                        insertUserIntoDatabase(email,fName, lName, id, password);
                         startActivity(new Intent(Register.this, Login.class));
                     }else{
                         Toast.makeText(Register.this, "Registration Error: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
@@ -75,5 +98,27 @@ public class Register extends AppCompatActivity {
             });
         }
     }
+
+    private void insertUserIntoDatabase(String email, String firstName, String lastName, String id, String password) {
+        Executors.newSingleThreadExecutor().execute(new Runnable() {
+            @Override
+            public void run() {
+                mDb.userDao().insert(new User(email, firstName, lastName, id, password));
+
+                List<User> userAccounts = mDb.userDao().getUsers();
+                for(User userAccount : userAccounts) {
+                    Log.d("Printing user emails" , userAccount.getEmail());
+                    Log.d("Printing user fname" , userAccount.getFirstName());
+                    Log.d("Printing userlnane" , userAccount.getLastName());
+                    Log.d("Printing user uid" , userAccount.getUserId());
+                    Log.d("Printing user pw" , userAccount.getPassword());
+
+                }
+
+            }
+        });
+
+    }
+
 
 }
---
+++ app/src/main/java/au/edu/unsw/infs3634/unswgamifiedlearningapp/User.java
@@ -0,0 +1,77 @@
+package au.edu.unsw.infs3634.unswgamifiedlearningapp;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class User {
+    @PrimaryKey
+    @NonNull
+    private String email;
+
+    @ColumnInfo(name = "first_name")
+    private String firstName;
+
+    @ColumnInfo(name = "last_name")
+    private String lastName;
+
+    @ColumnInfo(name = "userId")
+    private String userId;
+
+    @ColumnInfo(name = "password")
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public User(String email, String firstName, String lastName, String userId, String password){
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userId = userId;
+        this.password = password;
+    }
+
+    @NonNull
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(@NonNull String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+
+
+}
